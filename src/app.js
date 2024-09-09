0acf59986a19d293e1434726276d1da6ef8bb138
anatol-coder <ankawysota@gmail.com>
2024-09-09 14:37:03 +0100

    Updated Java Script

--- src/app.js
+++ src/app.js
@@ -53,6 +53,31 @@ function handleSearchSubmit(event) {
   searchCity(searchInputElement.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `<div class="weather-app-forecast-day">
+    <div class="weather-app-forecast-date">${day}</div>
+    <div class="weather-app-forecast-icon">🌤️</div>
+    <div class="weather-app-forecast-temperatures">
+      <div class="weather-app-forecast-temperature">
+        <strong>22°</strong>
+      </div>
+      <div class="weather-app-forecast-temperature">16°</div>
+    </div>
+    </div>
+`;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 searchCity("Tbilisi");
+displayForecast();
